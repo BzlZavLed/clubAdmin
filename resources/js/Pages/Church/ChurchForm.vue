acf0ed565fe4c64a0e8690a2ec085b59a8a3e6ce
benjaminzavala <benjamin@eadventist.net>
2026-01-13 15:29:31 -0500

    church form

--- resources/js/Pages/Church/ChurchForm.vue
+++ resources/js/Pages/Church/ChurchForm.vue
@@ -1,51 +1,56 @@
 <template>
-    <div class="p-4 max-w-lg mx-auto">
-        <form @submit.prevent="submitChurch">
-            <div class="mb-4">
-                <label class="block mb-1">Church Name *</label>
-                <input v-model="form.church_name" type="text" class="border p-2 w-full" required />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Address</label>
-                <input v-model="form.address" type="text" class="border p-2 w-full" />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Ethnicity</label>
-                <input v-model="form.ethnicity" type="text" class="border p-2 w-full" />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Phone Number</label>
-                <input v-model="form.phone_number" type="text" class="border p-2 w-full" />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Email</label>
-                <input v-model="form.email" type="email" class="border p-2 w-full" />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Pastor name</label>
-                <input v-model="form.pastor_name" type="text" class="border p-2 w-full" />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Pastor email</label>
-                <input v-model="form.pastor_email" type="email" class="border p-2 w-full" />
-            </div>
-            <div class="mb-4">
-                <label class="block mb-1">Conference</label>
-                <input v-model="form.conference" type="text" class="border p-2 w-full" />
-            </div>
-            <Link :href="route('register')" class="text-sm text-yellow-600 hover:underline">
-            Register staff
-            </Link><br>
+    <PathfinderLayout>
+        <template #title>Create Church</template>
 
-            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Submit</button>
+        <div class="p-4 max-w-lg mx-auto">
+            <form @submit.prevent="submitChurch">
+                <div class="mb-4">
+                    <label class="block mb-1">Church Name *</label>
+                    <input v-model="form.church_name" type="text" class="border p-2 w-full" required />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Address</label>
+                    <input v-model="form.address" type="text" class="border p-2 w-full" />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Ethnicity</label>
+                    <input v-model="form.ethnicity" type="text" class="border p-2 w-full" />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Phone Number</label>
+                    <input v-model="form.phone_number" type="text" class="border p-2 w-full" />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Email</label>
+                    <input v-model="form.email" type="email" class="border p-2 w-full" />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Pastor name</label>
+                    <input v-model="form.pastor_name" type="text" class="border p-2 w-full" />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Pastor email</label>
+                    <input v-model="form.pastor_email" type="email" class="border p-2 w-full" />
+                </div>
+                <div class="mb-4">
+                    <label class="block mb-1">Conference</label>
+                    <input v-model="form.conference" type="text" class="border p-2 w-full" />
+                </div>
+                <Link :href="route('register')" class="text-sm text-yellow-600 hover:underline">
+                Register staff
+                </Link><br>
 
-        </form>
-    </div>
+                <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded">Submit</button>
+
+            </form>
+        </div>
+    </PathfinderLayout>
 </template>
 <script setup>
 import { reactive } from 'vue'
 import axios from 'axios'
 import { Link } from '@inertiajs/vue3'
+import PathfinderLayout from '@/Layouts/PathfinderLayout.vue'
 
 const form = reactive({
     church_name: '',
